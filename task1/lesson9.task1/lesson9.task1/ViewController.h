b8ef378f0c3ba426368e33cd9ffc663bf32ed343
Anton Lookin <anton.lookin@gmail.com>
2015-12-24 09:48:05 +0200

    task1 empty project

---
+++ task1/lesson9.task1/lesson9.task1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lesson9.task1
+//
+//  Created by Anton Lookin on 12/24/15.
+//  Copyright © 2015 geekub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ task1/lesson9.task1/lesson9.task1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  lesson9.task1
+//
+//  Created by Anton Lookin on 12/24/15.
+//  Copyright © 2015 geekub. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
